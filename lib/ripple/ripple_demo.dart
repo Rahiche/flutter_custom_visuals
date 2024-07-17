afa4a5d26de243b27c6d75f45d9646a40afe84bb
Kevin Moore <kevmoo@google.com>
2024-07-17 16:47:01 -0700

    Update lints. Fix 'em

--- lib/ripple/ripple_demo.dart
+++ lib/ripple/ripple_demo.dart
@@ -1,7 +1,5 @@
 import 'dart:ui' as ui;
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_shaders/flutter_shaders.dart';
 
 class RippleDemoPage extends StatefulWidget {
