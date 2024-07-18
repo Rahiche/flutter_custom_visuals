b34d2f65be34073872e7a7882634a92d1d7c3e01
Raouf Rahiche <mrrahicheraouf@gmail.com>
2024-07-18 14:53:38 +0100

    add playground

--- lib/dynamic_displacement/dynamic_displacement_demo.dart
+++ lib/dynamic_displacement/dynamic_displacement_demo.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shaders/flutter_shaders.dart';
 
@@ -15,6 +18,7 @@ class _DynamicDisplacementDemoState extends State<DynamicDisplacementDemo>
   Offset _delta = const Offset(0, 0);
   late AnimationController _controller;
   late Animation<Offset> _animation;
+  final scrollController = ScrollController();
 
   @override
   void initState() {
@@ -68,26 +72,32 @@ class _DynamicDisplacementDemoState extends State<DynamicDisplacementDemo>
 
   @override
   Widget build(BuildContext context) {
-    final isMobile = MediaQuery.sizeOf(context).width < 500;
+    bool showSidebar = MediaQuery.sizeOf(context).width > 500;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onPanUpdate: _updatePosition,
-              onPanEnd: (_) => _animateToZero(),
-              child: addLayer(
-                child: Column(
-                  children: [
-                    Flexible(
-                      child: Padding(
-                        padding: EdgeInsets.all(isMobile ? 4 : 48.0),
-                        child: Container(
-                          color: Colors.black,
-                          child: const Text(
-                            """In realms where light and shadow play,
+      body: SafeArea(
+        child: Row(
+          children: [
+            if (showSidebar) buildPlayground(),
+            Expanded(
+              child: Stack(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: addLayer(
+                        child: GestureDetector(
+                          onPanUpdate: _updatePosition,
+                          onPanEnd: (_) => _animateToZero(),
+                          child: SingleChildScrollView(
+                            physics: const NeverScrollableScrollPhysics(),
+                            controller: scrollController,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                color: Colors.black,
+                                child: const Text(
+                                  """In realms where light and shadow play,
 Shaders weave the night and day,
 With code that dances, twists, and gleams,
 They craft the fabric of our dreams.
@@ -106,21 +116,208 @@ So here's to shaders, artists bold,
 With Flutter's tools, their stories told,
 In lines of code, they shape the skies,
 A digital dawn before our eyes.""",
-                            textAlign: TextAlign.center,
-                            overflow: TextOverflow.clip,
-                            style: TextStyle(
-                              fontSize: 35,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                                  textAlign: TextAlign.start,
+                                  overflow: TextOverflow.clip,
+                                  style: TextStyle(
+                                    fontSize: 35,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    // height: 2,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          FloatingActionButton(
+                            heroTag: "Down",
+                            child: const Text(
+                              "↓",
+                              style: TextStyle(fontSize: 40),
+                            ),
+                            onPressed: () {
+                              scrollController.animateTo(
+                                scrollController.offset + 250,
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.linearToEaseOut,
+                              );
+                            },
+                          ),
+                          const SizedBox(width: 12),
+                          FloatingActionButton(
+                            heroTag: "Up",
+                            child: const Text(
+                              "↑",
+                              style: TextStyle(fontSize: 40),
+                            ),
+                            onPressed: () {
+                              scrollController.animateTo(
+                                scrollController.offset - 250,
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.linearToEaseOut,
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPlayground() {
+    const minWidth = -1000.0;
+    const maxWidth = 1000.0;
+
+    const min = -1000.0;
+    const max = 1000.0;
+
+    return Expanded(
+      child: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Playground'),
+              const Divider(),
+              Text('Position.x ${_position.dx}'),
+              Slider(
+                value: _position.dx,
+                onChanged: (value) {
+                  setState(() {
+                    _position = Offset(value, _position.dy);
+                  });
+                },
+                min: min,
+                max: max,
+              ),
+              Text('Position.y ${_position.dy}'),
+              Slider(
+                value: _position.dy,
+                onChanged: (value) {
+                  setState(() {
+                    _position = Offset(_position.dx, value);
+                  });
+                },
+                min: min,
+                max: max,
+              ),
+              const Divider(),
+              Text('Delta.x ${_delta.dx}'),
+              Slider(
+                value: _delta.dx,
+                onChanged: (value) {
+                  setState(() {
+                    _delta = Offset(value, _delta.dy);
+                  });
+                },
+                min: min,
+                max: max,
+              ),
+              Text('Delta.y ${_delta.dy}'),
+              Slider(
+                value: _delta.dy,
+                onChanged: (value) {
+                  setState(() {
+                    _delta = Offset(_delta.dx, value);
+                  });
+                },
+                min: min,
+                max: max,
+              ),
+              const Divider(),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _position = Offset(
+                      min + (max - min) * Random().nextDouble(),
+                      min + (max - min) * Random().nextDouble(),
+                    );
+                    _delta = Offset(
+                      min + (max - min) * Random().nextDouble(),
+                      min + (max - min) * Random().nextDouble(),
+                    );
+                  });
+                },
+                child: const Text('Random Position and Delta'),
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () {
+                  const step = 20.0;
+                  const dxDelta = (max - min) / step;
+                  const dyDelta = (max - min) / step;
+                  setState(() {
+                    _position = const Offset(0, 0);
+                  });
+                  Timer.periodic(const Duration(milliseconds: 100), (timer) {
+                    if (_position.dx >= max && _position.dy >= max) {
+                      timer.cancel();
+                    } else {
+                      setState(() {
+                        _position = Offset(
+                          (_position.dx + dxDelta).clamp(min, max),
+                          (_position.dy + dyDelta).clamp(min, max),
+                        );
+                      });
+                    }
+                  });
+                },
+                child: const Text('Animate to Bottom Right'),
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () {
+                  const double max = 800; // maximum x position
+                  const double min = -200; // minimum x position
+
+                  setState(() {
+                    _delta = const Offset(100, -150);
+                    _position = const Offset(min, 0);
+                  });
+
+                  const dxDelta = 20.0;
+
+                  Timer.periodic(const Duration(milliseconds: 60), (timer) {
+                    if (_position.dy > 800) {
+                      timer.cancel();
+                    } else {
+                      setState(() {
+                        if (_position.dx >= max) {
+                          const double deltaY = 90;
+                          _position = Offset(min, _position.dy + deltaY);
+                        } else {
+                          _position = Offset(
+                            (_position.dx + dxDelta).clamp(min, max),
+                            _position.dy,
+                          );
+                        }
+                      });
+                    }
+                  });
+                },
+                child: const Text('Animate'),
+              ),
+              const SizedBox(height: 8),
+            ],
           ),
         ),
       ),
